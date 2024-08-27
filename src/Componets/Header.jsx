16195950a5689d5494ec99ebdfe85695400c28f6
Sounak Guin <sounak.guin@gmail.com>
2024-08-27 13:10:41 +0530

    first commit

--- src/Componets/Header.jsx
+++ src/Componets/Header.jsx
@@ -48,10 +48,22 @@ export default function Header() {
       <div className="container mx-auto p-4">
         <div className="flex flex-col-reverse md:flex-row gap-4">
           <div className="md:w-1/2 flex flex-col gap-4">
-            <h1 className="text-2xl font-bold">
-              SHREE HEMKUNT TYRES AND SERVICES{" "}
-              <span className="text-green-500">Verified</span>
-            </h1>
+            <div className="hidden md:block">
+              <div className="flex">
+                <p className="text-2xl font-bold">
+                  {" "}
+                  SHREE HEMKUNT TYRES AND SERVICES
+                </p>
+                <img
+                  src="/Images/5972778.png"
+                  alt="verified"
+                  className="h-4 w-4 mt-2 ml-5"
+                />
+                <span className="text-green-500 text-sm font-bold pt-2 pl-1">
+                  Verified
+                </span>
+              </div>
+            </div>
             <div className="flex items-center gap-2">
               <span className="bg-green-500 text-white rounded-full px-1 py-1 text-xs">
                 4.9
@@ -61,20 +73,23 @@ export default function Header() {
                   <span key={index}>★</span>
                 ))}
               </div>
-              <span className="text-gray-500">2278 Reviews</span>
+              <span className="text-gray-500 underline text-sm">
+                2278 Reviews
+              </span>
+              <div className="border border-gray-300 p-1 text-xs">Rate</div>
             </div>
             <p className="text-gray-600">
-              <FontAwesomeIcon icon={faLocationDot} className="pr-5" />
+              <FontAwesomeIcon icon={faLocationDot} className="pr-3" />
               PLOT NUMBER-09 GROUND FLOOR CISF CAMPUS ROAD, AHINSA KHAND-02
               <p className="md:pl-8">
                 INDIRAPURAM, Ghaziabad, Uttar Pradesh, 201014
               </p>
             </p>
             <p className="text-gray-600">
-              <FontAwesomeIcon icon={faClock} className="pr-5" />
+              <FontAwesomeIcon icon={faClock} className="pr-3" />
               Open - Monday to Sunday - 10:00AM to 8:00PM
             </p>
-            <button className="border text-red-500 border-red-500 font-bold w-64 hover:text-white py-2 px-4 rounded-md mt-4 hover:bg-red-600">
+            <button className="border text-red-500 border-red-500 font-bold w-52 hover:text-white py-2 px-4 rounded-md mt-4 hover:bg-red-600">
               Get Directions
             </button>
           </div>
@@ -124,13 +139,28 @@ export default function Header() {
                 />
               </div>
             </MultiCarousel>
+            <div className="block md:hidden">
+              <div className="flex">
+                <img
+                  src="/Images/5972778.png"
+                  alt="verified"
+                  className="h-4 w-4 mt-2 ml-1"
+                />
+                <span className="text-green-500 text-sm font-bold pt-2 pl-1">
+                  Verified
+                </span>
+              </div>
+              <p className="text-2xl font-bold pt-2">
+                SHREE HEMKUNT TYRES AND SERVICES
+              </p>
+            </div>
           </div>
         </div>
 
         <div>
-          <div className="flex">
-            <p>2278 </p>
-            <p>Google Reviews</p>
+          <div className="flex pt-5 font-bold">
+            <p className="">2278 </p>
+            <p className="pl-2">Google Reviews</p>
           </div>
           <MultiCarousel
             responsive={responsive1}
@@ -143,7 +173,6 @@ export default function Header() {
             draggable={true}
             removeArrowOnDeviceType={["tablet", "mobile"]}
             className="carousel"
-
           >
             {reviews.map((details, index) => (
               <div key={index} className="px-2">
